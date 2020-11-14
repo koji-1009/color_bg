c6aa89d5a92b02580187c16b8196deb06988128f
Koji Wakamiya <koji.wakamiya@gmail.com>
2020-11-15 01:16:57 +0900

    Change app name

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 
 @immutable
 class Constants {
-  static const String appName = 'Color BG';
+  static const String appName = 'Color BootCamp';
 
   static const String pageHome = '/home';
   static const String pageRgb = '/rgb';
