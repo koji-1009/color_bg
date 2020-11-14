5f0b8187d70bbf5f9413636f7ed882ff3fa57c37
Koji Wakamiya <koji.wakamiya@gmail.com>
2020-11-15 01:05:55 +0900

    Use bottomAppBar

--- lib/ui/rgb/rgb.dart
+++ lib/ui/rgb/rgb.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '../../constants.dart';
 import '../../data/model/color_value.dart';
 import '../../util/ext/color_value_ext.dart';
 import 'rgb_view_model.dart';
@@ -25,123 +24,137 @@ class RgbPage extends StatelessWidget {
         );
 
         return Scaffold(
-          appBar: AppBar(
-            title: const Text(Constants.appName),
-            actions: [
-              IconButton(
-                icon: const Icon(Icons.refresh),
-                onPressed: homeViewModel.changeColor,
-              ),
-            ],
-          ),
-          body: Center(
+          bottomNavigationBar: BottomAppBar(
             child: Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 24,
-                horizontal: 16,
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    height: _colorSize.height,
-                    width: _colorSize.width,
-                    color: question.color,
-                  ),
-                  const SizedBox(
-                    height: 24,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        'R',
-                        style: textStyle,
-                      ),
-                      Expanded(
-                        child: Slider(
-                          value: answer.r.toDouble(),
-                          min: 0,
-                          max: 255,
-                          label: answer.r.toString(),
-                          divisions: 256,
-                          onChanged: (value) {
-                            homeViewModel.update(r: value.round());
-                          },
-                        ),
-                      ),
-                      SizedBox(
-                        width: 40,
-                        child: Text(
-                          answer.r.toString(),
-                          style: textStyle,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        'G',
-                        style: textStyle,
-                      ),
-                      Expanded(
-                        child: Slider(
-                          value: answer.g.toDouble(),
-                          min: 0,
-                          max: 255,
-                          label: answer.g.toString(),
-                          divisions: 256,
-                          onChanged: (value) {
-                            homeViewModel.update(g: value.round());
-                          },
-                        ),
-                      ),
-                      SizedBox(
-                        width: 40,
-                        child: Text(
-                          answer.g.toString(),
-                          style: textStyle,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        'B',
-                        style: textStyle,
-                      ),
-                      Expanded(
-                        child: Slider(
-                          value: answer.b.toDouble(),
-                          min: 0,
-                          max: 255,
-                          label: answer.b.toString(),
-                          divisions: 256,
-                          onChanged: (value) {
-                            homeViewModel.update(b: value.round());
-                          },
-                        ),
-                      ),
-                      SizedBox(
-                        width: 40,
-                        child: Text(
-                          answer.b.toString(),
-                          style: textStyle,
-                        ),
-                      ),
-                    ],
-                  ),
-                  RaisedButton(
-                    child: const Text('Check answer'),
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  IconButton(
+                    icon: const Icon(Icons.arrow_back),
                     onPressed: () {
-                      _showResultSheet(
-                        context: context,
-                        question: question,
-                        answer: answer,
-                      );
+                      Navigator.pop(context);
                     },
                   ),
+                  IconButton(
+                    icon: const Icon(Icons.refresh),
+                    onPressed: homeViewModel.changeColor,
+                  ),
                 ],
+              ),
+            ),
+          ),
+          body: SafeArea(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 24,
+                  horizontal: 16,
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      height: _colorSize.height,
+                      width: _colorSize.width,
+                      color: question.color,
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'R',
+                          style: textStyle,
+                        ),
+                        Expanded(
+                          child: Slider(
+                            value: answer.r.toDouble(),
+                            min: 0,
+                            max: 255,
+                            label: answer.r.toString(),
+                            divisions: 256,
+                            onChanged: (value) {
+                              homeViewModel.update(r: value.round());
+                            },
+                          ),
+                        ),
+                        SizedBox(
+                          width: 40,
+                          child: Text(
+                            answer.r.toString(),
+                            style: textStyle,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'G',
+                          style: textStyle,
+                        ),
+                        Expanded(
+                          child: Slider(
+                            value: answer.g.toDouble(),
+                            min: 0,
+                            max: 255,
+                            label: answer.g.toString(),
+                            divisions: 256,
+                            onChanged: (value) {
+                              homeViewModel.update(g: value.round());
+                            },
+                          ),
+                        ),
+                        SizedBox(
+                          width: 40,
+                          child: Text(
+                            answer.g.toString(),
+                            style: textStyle,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          'B',
+                          style: textStyle,
+                        ),
+                        Expanded(
+                          child: Slider(
+                            value: answer.b.toDouble(),
+                            min: 0,
+                            max: 255,
+                            label: answer.b.toString(),
+                            divisions: 256,
+                            onChanged: (value) {
+                              homeViewModel.update(b: value.round());
+                            },
+                          ),
+                        ),
+                        SizedBox(
+                          width: 40,
+                          child: Text(
+                            answer.b.toString(),
+                            style: textStyle,
+                          ),
+                        ),
+                      ],
+                    ),
+                    RaisedButton(
+                      child: const Text('Check answer'),
+                      onPressed: () {
+                        _showResultSheet(
+                          context: context,
+                          question: question,
+                          answer: answer,
+                        );
+                      },
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
