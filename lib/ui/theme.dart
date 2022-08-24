e02c8466211252d06d5d5cbb73c33eb0d7ad35a4
Koji Wakamiya <koji.wakamiya@gmail.com>
2022-08-24 21:39:01 +0900

    Add poppins

--- lib/ui/theme.dart
+++ lib/ui/theme.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-ThemeData get lightTheme => ThemeData(
-      colorSchemeSeed: Colors.indigo,
-      brightness: Brightness.light,
-      useMaterial3: true,
-    );
+ThemeData get lightTheme {
+  final baseTheme = ThemeData(
+    colorSchemeSeed: Colors.indigo,
+    brightness: Brightness.light,
+    useMaterial3: true,
+  );
 
-ThemeData get darkTheme => ThemeData(
-      colorSchemeSeed: Colors.indigo,
-      brightness: Brightness.dark,
-      useMaterial3: true,
-    );
+  return baseTheme.copyWith(
+    textTheme: GoogleFonts.poppinsTextTheme(baseTheme.textTheme),
+  );
+}
+
+ThemeData get darkTheme {
+  final baseTheme = ThemeData(
+    colorSchemeSeed: Colors.indigo,
+    brightness: Brightness.dark,
+    useMaterial3: true,
+  );
+
+  return baseTheme.copyWith(
+    textTheme: GoogleFonts.poppinsTextTheme(baseTheme.textTheme),
+  );
+}
